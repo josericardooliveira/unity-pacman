9f3f902f701132a95f6a16f1b6c928da58b86b77
José Ricardo de Oliveira <josericardo.mbr@gmail.com>
2020-08-22 20:25:34 -0300

    Basic character movement and collisions

---
+++ Pacman/Assets/Scripts/PillScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PillScript : MonoBehaviour
+{
+    private Tilemap tilemap;
+
+    private void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        RemoveTile(collision);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RemoveTile(collision);
+    }
+
+    private void RemoveTile(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3Int cell = tilemap.WorldToCell(collision.transform.position);
+            tilemap.SetTile(cell, null);
+        }
+    }
+}
---
+++ Pacman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 moveDirection = Vector3.right * -1;
+
+    private Rigidbody2D rb;
+
+    public bool IsAlive;
+
+    public Animator animator;
+
+    private bool mvHoz = false;
+    private bool mvVer = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        rb.gravityScale = 0.0f;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        IsAlive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsAlive)
+        {
+            float xPos = 0, yPos = 0;
+            if (Input.GetAxis("Vertical") != 0 && Input.GetAxis("Horizontal") != 0)
+            {
+                if (mvHoz)
+                {
+                    yPos = Input.GetAxis("Vertical");
+                }
+                else if (mvVer)
+                {
+                    xPos = Input.GetAxis("Horizontal");
+                }
+            }
+            else
+            {
+                mvHoz = Input.GetAxis("Horizontal") != 0;
+                xPos = Input.GetAxis("Horizontal");
+                mvVer = Input.GetAxis("Vertical") != 0;
+                yPos = Input.GetAxis("Vertical");
+            }
+
+
+            if (xPos > 0
+                && !Physics2D.Raycast(transform.position + new Vector3(0.5f, 0.0f, 0.0f), Vector2.right, 0.5f)
+                && !Physics2D.Raycast(transform.position + new Vector3(0.5f, -0.25f, 0.0f), Vector2.right, 0.5f)
+                && !Physics2D.Raycast(transform.position + new Vector3(0.5f, 0.25f, 0.0f), Vector2.right, 0.5f))
+            {
+                moveDirection = Vector3.right;
+            }
+            else if (xPos < 0
+                && !Physics2D.Raycast(transform.position - new Vector3(0.5f, 0.0f, 0.0f), Vector2.left, 0.5f)
+                && !Physics2D.Raycast(transform.position - new Vector3(0.5f, -0.25f, 0.0f), Vector2.left, 0.5f)
+                && !Physics2D.Raycast(transform.position - new Vector3(0.5f, 0.25f, 0.0f), Vector2.left, 0.5f))
+            {
+                moveDirection = Vector3.right * -1;
+            }
+            else if (yPos > 0
+                && !Physics2D.Raycast(transform.position + new Vector3(0.0f, 0.5f, 0.0f), Vector2.up, 0.5f)
+                && !Physics2D.Raycast(transform.position + new Vector3(0.25f, 0.5f, 0.0f), Vector2.up, 0.5f)
+                && !Physics2D.Raycast(transform.position + new Vector3(-0.25f, 0.5f, 0.0f), Vector2.up, 0.5f))
+            {
+                moveDirection = Vector3.up;
+            }
+            else if (yPos < 0
+                && !Physics2D.Raycast(transform.position - new Vector3(0.0f, 0.5f, 0.0f), Vector2.down, 0.5f)
+                && !Physics2D.Raycast(transform.position - new Vector3(0.25f, 0.5f, 0.0f), Vector2.down, 0.5f)
+                && !Physics2D.Raycast(transform.position - new Vector3(-0.25f, 0.5f, 0.0f), Vector2.down, 0.5f))
+            {
+                moveDirection = Vector3.up * -1;
+            }
+
+            rb.velocity = moveDirection * 10;
+            animator.SetBool("IsAlive", IsAlive);
+            animator.SetFloat("XSpeed", moveDirection.x);
+            animator.SetFloat("YSpeed", moveDirection.y);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+}
