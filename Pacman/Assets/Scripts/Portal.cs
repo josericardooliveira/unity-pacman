6bef51d29b49e5a222f54d200b47d07e55163a48
José Ricardo de Oliveira <josericardo.mbr@gmail.com>
2020-08-23 13:16:30 -0300

    Add portal

---
+++ Pacman/Assets/Scripts/Portal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject otherPortal;
+
+    private Transform spawnPoint;
+
+    private void Start()
+    {
+        spawnPoint = otherPortal.transform.Find("Spawn").transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.position = spawnPoint.position;
+    }
+}
